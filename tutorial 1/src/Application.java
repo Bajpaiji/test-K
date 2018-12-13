301563afa32aa868c1c21c55c4c498b4af49da3e
Bajpaiji <anushree.bajpai@gmail.com>
2018-12-13 17:37:16 +0530

    my first commit

---
+++ tutorial 1/src/Application.java
@@ -0,0 +1,22 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		int mynumber=88;
+		byte mybyte=127;
+		short myshort = 1667;
+		long mylong = 78678897;
+		char mychar = 'h';
+		boolean mybool = true;
+		double mydouble = 7.2357;
+		float myfloat = 324.434f;
+		
+		
+		
+		System.out.println(mynumber);
+		System.out.println(myfloat);
+		System.out.println(mydouble);
+
+	}
+
+}
---
+++ tutorial 2/src/application.java
@@ -0,0 +1,17 @@
+
+public class application {
+	public static void main(String[] args) {
+		String text = "hello";
+		String space =" ";
+		String name = "bittu";
+		String greet = text + space + name;
+		int mynum = 17;
+		
+		System.out.println(greet);
+		System.out.println(text+space+name);
+		System.out.println("mynum is "+ mynum);
+		System.out.println(greet +space+ mynum);
+		
+	}
+
+}
